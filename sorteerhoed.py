38040fde7df9c608863c56f7f41c43c2404695cb
EncodedLoss <pepijndeoude@gmail.com>
2020-10-16 13:19:41 +0200

    adding files

---
+++ sorteerhoed.py
@@ -0,0 +1,79 @@
+from PyQt5.Qt import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import PyQt5.QtGui
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+import sys
+import json
+import time
+
+app = QApplication(sys.argv)
+
+question = ""
+vraag = "wil je dood?"
+
+
+
+iat = 0
+se = 0
+fict = 0
+bdm = 0
+iets = "hoiikbengijs"
+specialisatie = 0
+window = QWidget()
+window.setAutoFillBackground(True)
+layout = QGridLayout()
+window.setLayout(layout)
+qApp.setStyleSheet("QLabel { color: black; }");
+font = QtGui.QFont("Arial", 40)
+qApp.setFont(font)
+
+
+
+b1 = QPushButton("fuck1",)
+b2 = QPushButton("fuck 2")
+b3 = QPushButton("fuck3")
+b4 = QPushButton("fuck4")
+l1 = QLabel(vraag)
+
+
+
+window.setGeometry(200,200,500,300)
+b1.setMinimumHeight(80)
+p = window.palette()
+p.setColor(window.backgroundRole(), Qt.red)
+window.setPalette(p)
+layout.addWidget(l1, 2, 1,)
+layout.setColumnStretch(2 ,3)
+layout.addWidget(b1, 5, 0)
+layout.addWidget(b2, 3, 1)
+layout.addWidget(b3, 3, 2)
+layout.addWidget(b4, 3, 3)
+
+
+def color():
+    p.setColor(window.backgroundRole(), Qt.red)
+    window.setPalette(p)
+    time.sleep(3)
+    p.setColor(window.backgroundRole(), Qt.yellow)
+    window.setPalette(p)
+    time.sleep(3)
+    p.setColor(window.backgroundRole(), Qt.green)
+    window.setPalette(p)
+    time.sleep(3)
+
+#timer = QTimer()
+#timer.timeout.connect(color)
+#timer.start(10)
+
+window.show()
+app.exec_()
+
+for i in iets:
+    antwoord = input("zet ff 1 neer:")
+    if antwoord == 1:
+        specialisatie + 1
+    else:
+        specialisatie + 0
---
+++ sorteerhoed_2.0.py
@@ -0,0 +1,129 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.Qt import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import PyQt5.QtGui
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+import sys
+import json
+import time
+
+#INIT
+questionnr = 0
+sp1 = 0 #IAT
+sp2 = 0 #FICT
+sp3 = 0 #SE
+sp4 = 0 #BDAM
+
+f = open("vragen.json", "r")
+vragen = json.load(f)
+length = len(vragen["vragen"])
+#END INIT
+        
+vraag = "Ik vind het leuk om mij te verdiepen in de gebruikers."
+
+class Ui(QtWidgets.QDialog):
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('sorteerhoed.ui', self) # Load the .ui file
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.red)
+        self.setPalette(p)
+    
+        self.button1 = self.findChild(QtWidgets.QPushButton,"oneens")
+        self.button1.clicked.connect(self.printButtonPressed)
+        self.button2 = self.findChild(QtWidgets.QPushButton,"neutraal")
+        self.button2.clicked.connect(self.printButtonPressed)
+        self.button3 = self.findChild(QtWidgets.QPushButton,"eens")
+        self.button3.clicked.connect(self.printButtonPressed1)
+        self.button4 = self.findChild(QtWidgets.QPushButton,"back")
+        self.button4.clicked.connect(self.printButtonPressed2)
+
+        self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
+        self.label.setText(vraag)
+        self.label.setFont(font)
+
+        self.show()
+
+    def printButtonPressed(self): #Oneens/neutraal
+        global questionnr
+        f = open("vragen.json", "r")
+        vragen = json.load(f)
+        soort_vak = (vragen["vragen"][questionnr]["vak"])
+        #print(soort_vak)
+        vraag = (vragen["vragen"][questionnr]["vraag"])
+        #print(questionnr)
+        self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
+        self.label.setText(vraag)
+        self.label.setFont(font)
+        questionnr += 1
+    
+    def printButtonPressed1(self): #Eens
+        global questionnr
+        global sp1
+        global sp2
+        global sp3
+        global sp4
+
+        f = open("vragen.json", "r")
+        vragen = json.load(f)
+        soort_vak = (vragen["vragen"][questionnr]["vak"])
+        #print(soort_vak)
+        waarde = (vragen["vragen"][questionnr]["weging"])
+        vraag = (vragen["vragen"][questionnr]["vraag"])
+        #print(questionnr)
+        if soort_vak == "0":
+            sp1 = sp1 + int(waarde)
+        elif soort_vak == "1":
+            sp2 += int(waarde)
+        elif soort_vak == "2":
+            sp3 += int(waarde)
+        else:
+            sp4 += int(waarde)
+        print (sp1)
+        #print (sp2)
+        #print (sp3)
+        #print (sp4)
+        self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
+        self.label.setText(vraag)
+        self.label.setFont(font)
+        questionnr += 1
+    
+    def printButtonPressed2(self): #Back
+        global questionnr
+        global sp1
+        global sp2
+        global sp3
+        global sp4
+
+        questionnr -= 1
+        f = open("vragen.json", "r")
+        vragen = json.load(f)
+        soort_vak = (vragen["vragen"][questionnr]["vak"])
+        #print(soort_vak)
+        waarde = (vragen["vragen"][questionnr]["weging"])
+        vraag = (vragen["vragen"][questionnr]["vraag"])
+        #print(questionnr)
+        if soort_vak == "0":
+            sp1 -= int(waarde)
+        elif soort_vak == "1":
+            sp2 -= int(waarde)
+        elif soort_vak == "2":
+            sp3 -= int(waarde)
+        elif soort_vak == "3":
+            sp4 -= int(waarde)
+        print (sp1)
+        print (sp2)
+        print (sp3)
+        print (sp4)
+        self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
+        self.label.setText(vraag)
+        self.label.setFont(font)
+        questionnr += 1
+
+font = QtGui.QFont("MS Shell Dlg 2", 20)
+app = QtWidgets.QApplication(sys.argv) 
+window = Ui() 
+app.exec_() 
