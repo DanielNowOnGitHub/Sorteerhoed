ec94415aba47ea26a251f6ac46b0d0478c45f0e9
Patraklos2000 <72976276+Patraklos2000@users.noreply.github.com>
2020-10-16 17:28:47 +0200

    Update sorteerhoed_2.0.py

--- sorteerhoed_2.0.py
+++ sorteerhoed_2.0.py
@@ -22,7 +22,7 @@ vragen = json.load(f)
 length = len(vragen["vragen"])
 #END INIT
         
-vraag = "Ik vind het leuk om mij te verdiepen in de gebruikers."
+vraag = ""
 
 class Ui(QtWidgets.QDialog):
     def __init__(self):
@@ -30,9 +30,10 @@ class Ui(QtWidgets.QDialog):
         uic.loadUi('sorteerhoed.ui', self) # Load the .ui file
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.red)
-        self.setStyleSheet("QWidget#Vraagscherm1 {background-image : url(./harry.jpg);}")
+        #self.setStyleSheet("QWidget#Vraagscherm1 {background-image : url(./harry.jpg);}")
         self.setPalette(p)
-    
+      
+        
         self.button1 = self.findChild(QtWidgets.QPushButton,"oneens")
         self.button1.clicked.connect(self.printButtonPressed)
         self.button2 = self.findChild(QtWidgets.QPushButton,"neutraal")
@@ -40,7 +41,13 @@ class Ui(QtWidgets.QDialog):
         self.button3 = self.findChild(QtWidgets.QPushButton,"eens")
         self.button3.clicked.connect(self.printButtonPressed1)
         self.button4 = self.findChild(QtWidgets.QPushButton,"back")
-        self.button4.clicked.connect(self.printButtonPressed2)
+        self.button4.clicked.connect(self.exit_to_mainmenu)
+        self.tabs = self.findChild(QtWidgets.QTabWidget, "Vraagscherm")
+        self.startButton = self.findChild(QtWidgets.QPushButton,"start")
+        self.startButton.clicked.connect(self.startx)
+        self.ExitButton = self.findChild(QtWidgets.QPushButton, "exit_naar_home")
+        self.ExitButton.clicked.connect(self.exit_to_mainmenu)
+
 
         self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
         self.label.setText(vraag)
@@ -48,18 +55,42 @@ class Ui(QtWidgets.QDialog):
 
         self.show()
 
-    def printButtonPressed(self): #Oneens/neutraal
+    def startx(self):
         global questionnr
-        f = open("vragen.json", "r")
-        vragen = json.load(f)
-        soort_vak = (vragen["vragen"][questionnr]["vak"])
-        #print(soort_vak)
+        global sp1
+        global sp2
+        global sp3
+        global sp4
+        sp1 = 0 #IAT
+        sp2 = 0 #FICT
+        sp3 = 0 #SE
+        sp4 = 0 #BDAM
+        questionnr = 0
         vraag = (vragen["vragen"][questionnr]["vraag"])
-        #print(questionnr)
         self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
         self.label.setText(vraag)
         self.label.setFont(font)
         questionnr += 1
+
+        self.tabs.setCurrentIndex(1)
+
+
+    def printButtonPressed(self): #Oneens/neutraal
+        global questionnr
+        #f = open("vragen.json", "r")
+        #vragen = json.load(f)
+        #soort_vak = (vragen["vragen"][questionnr]["vak"])
+        #print(soort_vak)
+        if questionnr <= 14:
+            vraag = (vragen["vragen"][questionnr]["vraag"])
+            self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
+            self.label.setText(vraag)
+            self.label.setFont(font)
+            print(questionnr)
+            questionnr += 1
+        else: 
+            self.tabs.setCurrentIndex(2)
+
     
     def printButtonPressed1(self): #Eens
         global questionnr
@@ -68,29 +99,29 @@ class Ui(QtWidgets.QDialog):
         global sp3
         global sp4
 
-        f = open("vragen.json", "r")
-        vragen = json.load(f)
-        soort_vak = (vragen["vragen"][questionnr]["vak"])
-        #print(soort_vak)
-        waarde = (vragen["vragen"][questionnr]["weging"])
-        vraag = (vragen["vragen"][questionnr]["vraag"])
-        #print(questionnr)
-        if soort_vak == "0":
-            sp1 = sp1 + int(waarde)
-        elif soort_vak == "1":
-            sp2 += int(waarde)
-        elif soort_vak == "2":
-            sp3 += int(waarde)
+        
+        if questionnr <= 14:
+            soort_vak = (vragen["vragen"][questionnr]["vak"])
+            waarde = (vragen["vragen"][questionnr]["weging"])
+            vraag = (vragen["vragen"][questionnr]["vraag"])
+            if soort_vak == "0":
+                sp1 = sp1 + int(waarde)
+            elif soort_vak == "1":
+                sp2 += int(waarde)
+            elif soort_vak == "2":
+                sp3 += int(waarde)
+            else:
+                sp4 += int(waarde)
+            print (sp1)
+            #print (sp2)
+            #print (sp3)
+            #print (sp4)
+            self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
+            self.label.setText(vraag)
+            self.label.setFont(font)
+            questionnr += 1
         else:
-            sp4 += int(waarde)
-        print (sp1)
-        #print (sp2)
-        #print (sp3)
-        #print (sp4)
-        self.label = self.findChild(QtWidgets.QLabel,"vraagbox")
-        self.label.setText(vraag)
-        self.label.setFont(font)
-        questionnr += 1
+            self.tabs.setCurrentIndex(2)
     
     def printButtonPressed2(self): #Back
         global questionnr
@@ -100,8 +131,8 @@ class Ui(QtWidgets.QDialog):
         global sp4
 
         questionnr -= 1
-        f = open("vragen.json", "r")
-        vragen = json.load(f)
+        #f = open("vragen.json", "r")
+        #vragen = json.load(f)
         soort_vak = (vragen["vragen"][questionnr]["vak"])
         #print(soort_vak)
         waarde = (vragen["vragen"][questionnr]["weging"])
@@ -124,6 +155,8 @@ class Ui(QtWidgets.QDialog):
         self.label.setFont(font)
         questionnr += 1
 
+    def exit_to_mainmenu(self):     #naar startscherm
+        self.tabs.setCurrentIndex(0)
 font = QtGui.QFont("MS Shell Dlg 2", 20)
 app = QtWidgets.QApplication(sys.argv) 
 window = Ui() 
